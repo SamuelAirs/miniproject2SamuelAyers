e30edf38374d9b91934278eeec46410c53bb98d3
Samuel Ayers <sdayers@mail.fhsu.edu>
2024-02-19 09:47:55 -0600

    Commented out movies plots, changed to a different, better dataset

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ except FileExistsError:
     pass
 
 #create pandas dataframe called tvShows from csv data
-tvShows = pd.read_csv("IMDB.csv")
+booksPandasFrame = pd.read_csv("books.csv")
 
 """PLOT ONE, SHITTY"""
 
-tvShows.head(25)["Rating"].plot()
+#tvShows.head(25)["Rating"].plot()
 #savefile = "charts/Top 25 Rating.png"
 #plt.savefig(savefile)
 
@@ -30,31 +30,26 @@ tvShows.head(25)["Rating"].plot()
 #plt.show()
 
 """PLOT THREE, UNSTARTED"""
-tvShows.head(25)["Rating"].plot.density()
-plt.show()
+#tvShows.head(25)["Rating"].plot.density()
+#plt.show()
 
 """PLOT FOUR, FINISHED"""
 #pulls top five tv shows from pandaframe
-topFiveTvShows = tvShows.head(5)
-
+#topFiveTvShows = tvShows.head(5)
 #finds min and max rating and creates buffer
-minRating = topFiveTvShows['Rating'].min() - .1
-maxRating = topFiveTvShows['Rating'].max() + .1
-
+#minRating = topFiveTvShows['Rating'].min() - .1
+#maxRating = topFiveTvShows['Rating'].max() + .1
 #creates bar plot
-topFiveTvShows.plot.bar(x='Name', y='Rating', legend=False, figsize=(8, 16))
-
+#topFiveTvShows.plot.bar(x='Name', y='Rating', legend=False, figsize=(8, 16))
 #adds labels
-plt.title('Top 5 Tv Shows by Rating', fontsize='32')
-
+#plt.title('Top 5 Tv Shows by Rating', fontsize='32')
 #customize plot
-plt.xticks(rotation=45, fontsize='12')
-plt.ylim(minRating, maxRating)
-
+#plt.xticks(rotation=45, fontsize='12')
+#plt.ylim(minRating, maxRating)
 #save file
 #savefile = "charts/Top 5 Tv Shows By Rating.png"
 #plt.savefig(savefile)
 
 """PLOT FIVE, UNSTARTED"""
-tvShows.plot(tvShows["Episodes"], tvShows["Rating"])
-plt.show()
+#tvShows.plot(tvShows["Episodes"], tvShows["Rating"])
+#plt.show()
