6e297aae999eb636e2590d6b40f334daafe78554
SamuelAirs <sdayers@mail.fhsu.edu>
2024-02-18 21:37:57 -0600

    commented out save for now, made try exception for if charts folder already exists, added charts to .gitignore

--- main.py
+++ main.py
@@ -9,16 +9,19 @@ from pathlib import Path
 
 
 #create charts folder
-#Path("charts").mkdir()
+try:
+    Path("charts").mkdir()
+except FileExistsError:
+    pass
 
 #create pandas dataframe called tvShows from csv data
 tvShows = pd.read_csv("IMDB.csv", index_col=0)
 
 #PLOT ONE, SHITTY
-#tvShows.head(25)["Rating"].plot()
+tvShows.head(25)["Rating"].plot()
 #savefile = "charts/Top 25 Rating.png"
 #plt.savefig(savefile)
-#plt.show()
+plt.show()
 
 #PLOT TWO, UNSTARTED
 
